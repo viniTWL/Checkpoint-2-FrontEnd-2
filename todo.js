f425cd655b3eec815a3c836bbb2bb046e9b28826
viniTWL <vinigdc12@gmail.com>
2021-11-23 16:06:23 -0300

    creating form
    Criando o formulário

---
+++ todo.js
@@ -0,0 +1,12 @@
+btnSub = document.querySelector('#btnSub');
+dataCriacao = document.querySelector('#dataCriacao');
+dataLimite = document.querySelector('#dataLimite');
+description = document.querySelector('#description');
+
+//btnSub.addEventListener('click', (submit) => {}) 
+
+
+/* function pegarDataAtual(){
+    data = new Date();
+    document.getElementById('data').value = data.getDay()+'/'+data.getMonth()+'/'+data.getFullYear();
+ } */
