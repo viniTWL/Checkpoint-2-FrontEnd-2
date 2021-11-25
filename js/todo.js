34e26db32ac1e2d76b85216abd47cd25082a4a20
viniTWL <vinigdc12@gmail.com>
2021-11-25 14:31:29 -0300

    first commit js
    Adicionanda funções, verificação e evento

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,69 @@
-btnSub = document.querySelector('#btnSub');
-dataCriacao = document.querySelector('#dataCriacao');
-dataLimite = document.querySelector('#dataLimite');
-description = document.querySelector('#description');
+// Selecionando os inputs do form
 
-//btnSub.addEventListener('click', (submit) => {}) 
+const btnSub = document.querySelector('#btnSubmit');
+const dataLimite = document.querySelector('#datalimite');
+const task = document.querySelector('#tarefa');
+const list = document.querySelector('#list');
 
+// Função que pega a data atual e formata com o padrão ISO
 
-/* function pegarDataAtual(){
+let AtualDate = () => {
     data = new Date();
-    document.getElementById('data').value = data.getDay()+'/'+data.getMonth()+'/'+data.getFullYear();
- } */
+    let today = data.getFullYear() +'-'+ (data.getMonth() + 1)  +'-'+ data.getDate();
+    return today;
+ }
+
+ // Função que cria um novo card, o elemento colocado no parametro será o pai.
+
+ let newCard = (element) => {
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'task')
+
+    li.innerHTML = `
+                            <div class="title">
+                                <p class="taskanme">${task.value}</p>
+                            </div>
+                            <div class="dateCreate">
+                                <p class="dateTask">Criado em: ${AtualDate()}</p>
+                            </div>
+                            <div class="dateEnd">
+                                <p class="date">Terminar em: ${dataLimite.value}</p>
+                            </div>
+                         `
+    element.appendChild(li);
+}
+
+// Evento quando inicia a página, a data limite do input date é setado como a data atual
+
+window.addEventListener('DOMContentLoaded', function() {
+    dataLimite.min = AtualDate();
+});
+
+// Evento do botão submit, veriica-se se os campos estão em branco, se a tarefa tem mesno de 10 caracteres
+// Se sim, mensagem de erro, se não, cria a tarefa com a função newCard.
+
+btnSub.addEventListener('click', submit => {
+
+    submit.preventDefault();
+
+    let verify = false;
+
+    if(dataLimite.value == '' || task.value == ''){
+        alert('Preencha todos os campos!')
+        verify = false;
+    }else{
+        verify = true;
+    }
+
+    if(task.value.length <= 10){
+            alert('A descrição deve ter pelo menos 10 caracteres')
+            verify = false;
+    }else{
+        verify = true;
+    }
+
+    if(verify){
+        newCard(list)
+    }
+}) 
